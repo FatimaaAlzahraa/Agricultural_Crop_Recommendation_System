3a2e42e2176f440e2f77017fd342aa5266b1adda
zahraa <ftoomrrt@gmail.com>
2025-08-31 22:41:29 +0300

    clean

--- clean_notebook.py
+++ clean_notebook.py
@@ -1,19 +1,18 @@
 import nbformat
 
-# اسم ملف النوتبوك الأصلي
+
 notebook_path = "Agricultural Crop Recommendation System using Clustering and Classification.ipynb"
 output_path = "Agricultural_Crop_Recommendation_System_clean.ipynb"
 
-# اقرأ النوتبوك
+
 with open(notebook_path, "r", encoding="utf-8") as f:
     nb = nbformat.read(f, as_version=4)
 
-# لو فيه widgets في metadata شيلها
 if "widgets" in nb.metadata:
     print("🧹 Removing widgets metadata...")
     del nb.metadata["widgets"]
 
-# احفظ نسخة جديدة نظيفة
+
 with open(output_path, "w", encoding="utf-8") as f:
     nbformat.write(nb, f)
 
